ee059a808ca8ec7fb56267a742aacf1c9c65d185
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2019-12-19 16:43:32 +0100

    NavBar responsive added

--- views/shared/_headerBig.php
+++ views/shared/_headerBig.php
@@ -14,12 +14,54 @@
 -->
     <nav>
         <ul>
-            <li><a href="index.php?a=index"><img src="assets/images/homeIcon.png"  alt="Home Bild"><br></br>HOME</a></li>
-            <li><a href="index.php?a=index"><img src="assets/images/boyIcon.png"  alt="Jungen Bild"><br></br>JUNGEN</a></li>
-            <li><a href="index.php?a=index"><img src="assets/images/girlIcon.png"  alt="Mädchen Bild"><br></br>MÄDCHEN</a></li>
-            <li><a href="index.php?a=index"><img src="assets/images/controllerIcon.png"  alt="Konsolen Bild"><br></br>KONSOLEN</a></li>
-            <li><a href="index.php?a=index"><img src="assets/images/headIcon.png"  alt="Konto Bild"><br></br>KONTO</a></li>
-            <li><a href="index.php?a=index"><img src="assets/images/aboutIcon.png"  alt="Über uns Bild"><br></br>ÜBER UNS</a></li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/homeIcon.png"  alt="Home Bild">
+                        <figcaption>HOME</figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/boyIcon.png"  alt="Jungen Bild">
+                        <figcaption>JUNGEN</figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/girlIcon.png"  alt="Mädchen Bild">
+                        <figcaption>MÄDCHEN</figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/iconController.png"  alt="Konsolen Bild">
+                        <figcaption>KONSOLEN</figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/headIcon.png"  alt="Konto Bild">
+                        <figcaption>KONTO</figcaption>
+                    </figure>
+                </a>
+            </li>
+            <li>
+                <a href="index.php?a=index">
+                    <figure class="single">
+                        <img src="assets/images/aboutIcon.png"  alt="Über uns Bild">
+                        <figcaption>ÜBER UNS</figcaption>
+                    </figure>
+                </a>
+            </li>
         </ul>
     </nav>
 </header>
