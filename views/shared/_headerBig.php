4cd484317bf2c4fa91c562d9191176c774c93f5b
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2019-12-19 16:18:38 +0100

    headBig und css changed

--- views/shared/_headerBig.php
+++ views/shared/_headerBig.php
@@ -2,11 +2,25 @@
   <a href="index.php"><img id="logo" src="assert/photos/Logo_Klein.png" alt="Toyplanet_logo"></a>
   <input id="search" type="text" placeholder="Search">
 
+    <!--
     <div class="pill-nav">
-        <a href= "index.php?a=index" ><img id="homeIcon" src="assets/images/homeIcon.png"  alt="Home"></a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a href="index.php?a=index"><img src="assets/images/homeIcon.png"  alt="Home Bild"><br></br>HOME</a>
+        <a href="index.php?a=index"><img src="assets/images/boyIcon.png"  alt="Jungen Bild"><br></br>JUNGEN</a>
+        <a href="index.php?a=index"><img src="assets/images/girlIcon.png"  alt="Mädchen Bild"><br></br>MÄDCHEN</a>
+        <a href="index.php?a=index"><img src="assets/images/controllerIcon.png"  alt="Konsolen Bild"><br></br>KONSOLEN</a>
+        <a href="index.php?a=index"><img src="assets/images/headIcon.png"  alt="Konto Bild"><br></br>KONTO</a>
+        <a href="index.php?a=index"><img src="assets/images/aboutIcon.png"  alt="Über uns Bild"><br></br>ÜBER UNS</a>
     </div>
+-->
+    <nav>
+        <ul>
+            <li><a href="index.php?a=index"><img src="assets/images/homeIcon.png"  alt="Home Bild"><br></br>HOME</a></li>
+            <li><a href="index.php?a=index"><img src="assets/images/boyIcon.png"  alt="Jungen Bild"><br></br>JUNGEN</a></li>
+            <li><a href="index.php?a=index"><img src="assets/images/girlIcon.png"  alt="Mädchen Bild"><br></br>MÄDCHEN</a></li>
+            <li><a href="index.php?a=index"><img src="assets/images/controllerIcon.png"  alt="Konsolen Bild"><br></br>KONSOLEN</a></li>
+            <li><a href="index.php?a=index"><img src="assets/images/headIcon.png"  alt="Konto Bild"><br></br>KONTO</a></li>
+            <li><a href="index.php?a=index"><img src="assets/images/aboutIcon.png"  alt="Über uns Bild"><br></br>ÜBER UNS</a></li>
+        </ul>
+    </nav>
 </header>
    
