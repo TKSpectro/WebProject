7eda3926f64bbe92fd91d6fdb2a51f9a48657b41
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2019-12-19 15:44:37 +0100

    add some Icons and change some

--- views/shared/_headerBig.php
+++ views/shared/_headerBig.php
@@ -1,5 +1,12 @@
 <header class="header_big">
   <a href="index.php"><img id="logo" src="assert/photos/Logo_Klein.png" alt="Toyplanet_logo"></a>
   <input id="search" type="text" placeholder="Search">
+
+    <div class="pill-nav">
+        <a href= "index.php?a=index" ><img id="homeIcon" src="assets/images/homeIcon.png"  alt="Home"></a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
 </header>
    
