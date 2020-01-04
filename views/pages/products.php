679a6483f909818b444c4c23ff9a5a84f24db923
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-04 14:47:00 +0100

    remove boy etc to move into products

---
+++ views/pages/products.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+
+<div class="productList">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <h3>Card 1</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <h3>Card 2</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <h3>Card 3</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <h3>Card 4</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+            </div>
+        </div>
+    </div>
+</div>
