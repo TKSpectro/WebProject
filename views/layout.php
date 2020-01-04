3c234fb324cd69706af88b11b192bd3567303aed
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-04 14:53:23 +0100

    Change import type

--- views/layout.php
+++ views/layout.php
@@ -2,9 +2,6 @@
 <html lang="de">
 <head>
 <link rel="stylesheet" type="text/css" href="assets/css/Toyplanet_style.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/productList.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/sideNav.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/topNav.css">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
