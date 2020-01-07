21b2df9fd94d234fdd5af5d155de177ed77ece39
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-01-07 13:47:37 +0100

    .

--- models/cat.class.php
+++ models/cat.class.php
@@ -8,11 +8,10 @@ class Cat extends BaseModel
     const TABLENAME = '`cat`';
 
     protected $schema = [
-       
         'catID'       => ['type' => BaseModel::TYPE_INT],
         'createdAt'    => ['type' => BaseModel::TYPE_STRING],
         'updatedAt'    => ['type' => BaseModel::TYPE_STRING],
-        'descrip'      => ['type' => BaseModel::TYPE_STRING],
+        'descrip'      => ['type' => BaseModel::TYPE_STRING]
     ];
 }
 
