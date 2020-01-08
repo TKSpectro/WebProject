3e1e0bf36c74774ca1a9753dd75eb9a58699faed
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-01-08 18:42:08 +0100

    .

---
+++ views/pages/warenkorb.php
@@ -0,0 +1,10 @@
+
+<form  action=""  methode="GET">
+<input type="submit" name="addButton" > </form>
+<?php if(isset($_GET['addButton']))
+    {echo 'huhuhuhu';}
+    else
+    echo 'Nichts';
+
+    ?>
+
