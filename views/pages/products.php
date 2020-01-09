e0ebbe4961e3ceb68e92b6cfb3deb7015fdf2609
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-09 15:01:25 +0100

    Update products.php

--- views/pages/products.php
+++ views/pages/products.php
@@ -3,51 +3,57 @@ namespace app\models;
 $productCounter = 0;
 ?>
 <div class="productList">
-<ul>
-
-<?php if(isset($_GET['cat'])) 
-{
-    foreach (ProdCat::find( 'catID = "'. $_GET['cat']. '"' ) as $prodCat)
-    {
-    foreach (Product::find( 'prodCatID = "'. $prodCat['prodCatID']. '"' ) as $prod):?> 
-    <div class="column">
-        <li><div class="card">
-        <img  src="<?=$prod['photo']?>" alt="produckt">
-        <h3><?= $prod['prodCatID']?></h3>
-        <p><?= $prod['descrip'] ?></p>
-        <p><?= $prod['stdPrice'] ?>€</p>
-        </div></li>
-    </div> 
-<?endforeach; }?>
-
-<?php } 
-elseif(isset($_GET['prodCat'])) 
-{foreach (Product::find( 'prodCatID = "'. $_GET['prodCat']. '"' ) as $prod):?>
-<div class="column">
-    <li><div class="card">
-    <img  src="<?=$prod['photo']?>" alt="produckt">
-        <h3><?= $prod['prodID'] ?></h3>
-        <p><?= $prod['descrip'] ?></p>
-        <p><?= $prod['stdPrice'] ?>€</p>
-</div></li>
-</div> 
-<?endforeach; ?>
-    <?php }
-    else
-    { foreach ($product as $prod):?>
-    <div class="column">
-        <li><div class="card">
-         
-            <img  src="<?=$prod['photo']?>" alt="produckt">
-            <h3><?= $prod['prodID'] ?></h3>
-            <p><?= $prod['descrip'] ?></p>
-            <p><?= $prod['stdPrice'] ?>€</p>
-            <form  action="" >
-            <input type="submit" value="Add To Warnkorb" > </form>
-    </div></li>
-</div> 
-<?endforeach; }?>
-
-
-</ul>
+    <ul>
+        <?php if (isset($_GET['cat']))
+        {
+            foreach (ProdCat::find('catID = "' . $_GET['cat'] . '"') as $prodCat)
+            {
+                foreach (Product::find('prodCatID = "' . $prodCat['prodCatID'] . '"') as $prod):?>
+                    <div class="column">
+                        <li>
+                            <div class="card">
+                                <img src="<?= $prod['photo'] ?>" alt="produckt">
+                                <h3><?= $prod['prodCatID'] ?></h3>
+                                <p><?= $prod['descrip'] ?></p>
+                                <p><?= $prod['stdPrice'] ?>€</p>
+                            </div>
+                        </li>
+                    </div>
+                <? endforeach;
+            } ?>
+        <?php }
+        elseif (isset($_GET['prodCat']))
+        {
+            foreach (Product::find('prodCatID = "' . $_GET['prodCat'] . '"') as $prod):?>
+                <div class="column">
+                    <li>
+                        <div class="card">
+                            <img src="<?= $prod['photo'] ?>" alt="produckt">
+                            <h3><?= $prod['prodID'] ?></h3>
+                            <p><?= $prod['descrip'] ?></p>
+                            <p><?= $prod['stdPrice'] ?>€</p>
+                        </div>
+                    </li>
+                </div>
+            <? endforeach; ?>
+        <?php }
+        else
+        {
+            foreach ($product as $prod): ?>
+                <div class="column">
+                    <li>
+                        <div class="card">
+                            <img src="<?= $prod['photo'] ?>" alt="produckt">
+                            <h3><?= $prod['prodID'] ?></h3>
+                            <p><?= $prod['descrip'] ?></p>
+                            <p><?= $prod['stdPrice'] ?>€</p>
+                            <form>
+                                <input type="submit" value="Add To Warnkorb">
+                            </form>
+                        </div>
+                    </li>
+                </div>
+            <? endforeach;
+        } ?>
+    </ul>
 </div>
