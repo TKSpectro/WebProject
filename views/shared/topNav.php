b7c0c7ad9d990f3b3b7e6169c4a2ca4694ec4dba
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-09 12:44:48 +0100

    Update topNav.php
    removed old topNav

--- views/shared/topNav.php
+++ views/shared/topNav.php
@@ -38,46 +38,4 @@ namespace app\models;
         <button class="subnavbtn"><img src="assets/images/aboutIcon.png" alt="Über uns Bild">Über Uns</button>
     </div>
 </div>
-
-
-<!--<nav class="topNav">
-    <ul>
-
-        <li>
-            <a href="index.php?a=products&type=Jungs-Toys">
-                <figure class="single">
-                    <img src="assets/images/boyIcon.png" alt="Jungen Bild">
-                    <figcaption id="junge">JUNGEN</figcaption>
-                </figure>
-            </a>
-        </li>
-        <li>
-            <a href="index.php?a=products&type=Konsolespiele">
-                <figure class="single">
-                    <img src="assets/images/iconController.png" alt="Konsolen Bild">
-                    <figcaption id="konsole">KONSOLEN</figcaption>
-                </figure>
-            </a>
-        </li>
-        <li>
-            <a href="index.php?a=products&type=Mädchen-Toys">
-                <figure class="single">
-                    <img src="assets/images/iconGirl.png" alt="Mädchen Bild">
-                    <figcaption id="mädchen">MÄDCHEN</figcaption>
-                </figure>
-            </a>
-        </li>
-
-
-        <li>
-            <a href="index.php?a=index">
-                <figure class="single">
-                    <img src="assets/images/aboutIcon.png" alt="Über uns Bild">
-                    <figcaption id="überUns">ÜBER UNS</figcaption>
-                </figure>
-            </a>
-        </li>
-    </ul>
-</nav>-->
-
    
