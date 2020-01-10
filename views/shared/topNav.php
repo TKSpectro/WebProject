2b94913a3455110216a6510b050c2be469f85896
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-01-10 18:31:02 +0100

    topNav

--- views/shared/topNav.php
+++ views/shared/topNav.php
@@ -3,8 +3,8 @@ namespace app\models;
 ?>
 <nav>
     <ul class="navbar">
-        <li class="subnav">
-            <button class="subnavbtn"><a href="index.php?a=products&type=Jungs-Toys"><img src="assets/images/boyIcon.png" alt="Jungen Bild"></a>Jungen</button>
+        <li class="subnav" id="test">
+            <a href="index.php?a=products&type=Jungs-Toys" class="subnavbtn"><img src="assets/images/boyIcon.png" alt="Jungen Bild"></a><div id="topNavBoy">Jungen</div>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "boy" OR type ="both"') as $Cat)
                 {
@@ -15,7 +15,7 @@ namespace app\models;
             </ul>
         </li>
         <li class="subnav">
-            <button class="subnavbtn"><a href="index.php?a=products&type=Konsolespiele"><img src="assets/images/controllerIcon.png" alt="Konsolen Bild"></a>Konsolen</button>
+            <a href="index.php?a=products&type=Konsolespiele" class="subnavbtn"><img src="assets/images/controllerIcon.png" alt="Konsolen Bild"></a><div id="topNavKonsole">Konsolen</div>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "console"') as $Cat)
                 {
@@ -26,7 +26,7 @@ namespace app\models;
             </ul>
         </li>
         <li class="subnav">
-            <button class="subnavbtn"><a href="index.php?a=products&type=Mädchen-Toys"><img src="assets/images/iconGirl.png" alt="Mädchen Bild"></a>Mädchen</button>
+            <a href="index.php?a=products&type=Mädchen-Toys" class="subnavbtn"><img src="assets/images/iconGirl.png" alt="Mädchen Bild"></a><div id="topNavGirl"> Mädchen</div>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "girl" OR type ="both"') as $Cat)
                 {
@@ -38,7 +38,7 @@ namespace app\models;
             </ul>
         </li>
         <li class="subnav">
-            <button class="subnavbtn"><img src="assets/images/aboutIcon.png" alt="Über uns Bild">Über Uns</button>
+            <a href="" class="subnavbtn"><img src="assets/images/aboutIcon.png" alt="Über uns Bild"></a><div id="topNavInfo">Über Uns</div>
         </li>
     </ul>
 </nav>
