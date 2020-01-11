22982847637abb7a77e3ef1986595a8493eb9847
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-01-11 15:52:48 +0100

    .

--- views/pages/shoppingCart.php
+++ views/pages/shoppingCart.php
@@ -1,16 +1,26 @@
 <?php 
-namespace app\models;
-foreach ($ShoppingCartProduct as $prod)
-{
-    $test= Product::find('prodID = "' . $prod['prodID'] . '"');    ?>
-    <div class="column">
-        <li>
-            <div class="card">
-                <img src="<?= $test['0']['photo'] ?>" alt="produckt">
-                <p><?= $test['0']['descrip'] ?></p>
-                <p><?= $test['0']['stdPrice'] ?>€</p>
-            </div>
-        </li>
-    </div>
-<?}?>
-
+namespace app\models;?>
+<div class="Shoppingcart">
+<h1>MEIN SHOPPING CART</h1>
+<table>
+    <div class="tableRow">
+    <tr>
+        <th>PRODUCT</th>
+        <th>PRICE</th>
+        <th>QUANTITY</th>
+        <th>TOTAL</th>
+    </tr></div>
+ 
+    <?php foreach ($ShoppingCartProduct as $prodID)
+{ $produkt= Product::find('prodID = "' . $prodID['prodID'] . '"');   
+  $prod=$produkt['0']?>
+    <tr class="tableRow">
+    <td><img src="<?= $prod['photo'] ?>" alt="produckt">
+   <?= $prod['descrip'] ?></p></td>
+    <td><p><?= $prod['stdPrice'] ?>€</td>
+    <td>1</td>
+    <td>22</td>
+<?}?>       
+</tr>
+</table>
+</div>
