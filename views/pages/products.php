b59e690bf4c2f230f64fd2904a02c411f1df476d
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-12 12:00:17 +0100

    rework topNav a bit and add search functionality

--- views/pages/products.php
+++ views/pages/products.php
@@ -9,20 +9,27 @@ $productCounter = 0;
             foreach (ProdCat::find('catID = "' . $_GET['cat'] . '"') as $prodCat)
             {
                 foreach (Product::find('prodCatID = "' . $prodCat['prodCatID'] . '"') as $prod)
-                include __DIR__.'/../shared/productLook.php';
-            } 
+                    include __DIR__ . '/../shared/productLook.php';
+            }
         }
         elseif (isset($_GET['prodCat']))
         {
             foreach (Product::find('prodCatID = "' . $_GET['prodCat'] . '"') as $prod)
-               include __DIR__.'/../shared/productLook.php';
+                include __DIR__ . '/../shared/productLook.php';
         }
+
         else
         {
-            foreach ($product as $prod) 
-            include __DIR__.'/../shared/productLook.php';
-
-             
+            if (isset($_GET['search']))
+            {
+                foreach (Product::find('descrip like "' . '%' . $_GET['search'] . '%' . '"') as $prod)
+                    include __DIR__ . '/../shared/productLook.php';
+            }
+            else
+            {
+                foreach ($product as $prod)
+                    include __DIR__ . '/../shared/productLook.php';
+            }
         } ?>
     </ul>
 </div>
--- views/shared/topNav.php
+++ views/shared/topNav.php
@@ -3,42 +3,53 @@ namespace app\models;
 ?>
 <nav>
     <ul class="navbar">
-        <li class="subnav" id="test">
-            <a href="index.php?a=products&type=Jungs-Toys" class="subnavbtn"><img src="assets/images/boyIcon.png" alt="Jungen Bild"></a><div id="topNavBoy">Jungen</div>
+        <li class="subnav paddLeft">
+            <a href="index.php?a=products&type=Jungs-Toys" class="subnavbtn"><img src="assets/images/boyIcon.png"
+                                                                                  alt="Jungen Bild">
+                <div class="topNavBoy <? if($_GET['type'] == "Jungs-Toys"){?>activeUnderline<?}?>">Jungen</div>
+            </a>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "boy" OR type ="both"') as $Cat)
                 {
                     ?>
-                    <li><a href="index.php?a=products&type=Jungs-Toys&cat=<?= $Cat['catID'] ?>">
+                    <li><a class="topNavBoy" href="index.php?a=products&type=Jungs-Toys&cat=<?= $Cat['catID'] ?>">
                             <?= $Cat['descrip']; ?></a></li>
                 <? } ?>
             </ul>
         </li>
         <li class="subnav">
-            <a href="index.php?a=products&type=Konsolespiele" class="subnavbtn"><img src="assets/images/controllerIcon.png" alt="Konsolen Bild"></a><div id="topNavKonsole">Konsolen</div>
+            <a href="index.php?a=products&type=Konsolespiele" class="subnavbtn"><img
+                        src="assets/images/controllerIcon.png" alt="Konsolen Bild">
+                <div class="topNavConsole <? if($_GET['type'] == "Konsolespiele"){?>activeUnderline<?}?>">Konsolen</div>
+            </a>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "console"') as $Cat)
                 {
                     ?>
-                    <li><a href="index.php?a=products&type=Konsolespiele&cat=<?= $Cat['catID'] ?>">
+                    <li><a class="topNavConsole"
+                           href="index.php?a=products&type=Konsolespiele&cat=<?= $Cat['catID'] ?>">
                             <?= $Cat['descrip']; ?></a></li>
                 <? } ?>
             </ul>
         </li>
         <li class="subnav">
-            <a href="index.php?a=products&type=Mädchen-Toys" class="subnavbtn"><img src="assets/images/iconGirl.png" alt="Mädchen Bild"></a><div id="topNavGirl"> Mädchen</div>
+            <a href="index.php?a=products&type=Mädchen-Toys" class="subnavbtn"><img src="assets/images/iconGirl.png"
+                                                                                    alt="Mädchen Bild">
+                <div class="topNavGirl <? if($_GET['type'] == "Mädchen-Toys"){?>activeUnderline<?}?>">Mädchen</div>
+            </a>
             <ul class="subnav-content">
                 <? foreach (Cat::find('type = "girl" OR type ="both"') as $Cat)
                 {
                     ?>
-                    <li><a href="index.php?a=products&type=Mädchen-Toys&cat=<?= $Cat['catID'] ?>">
+                    <li><a class="topNavGirl" href="index.php?a=products&type=Mädchen-Toys&cat=<?= $Cat['catID'] ?>">
                             <?= $Cat['descrip']; ?></a></li>
                 <? } ?>
-
             </ul>
         </li>
         <li class="subnav">
-            <a href="" class="subnavbtn"><img src="assets/images/aboutIcon.png" alt="Über uns Bild"></a><div id="topNavInfo">Über Uns</div>
+            <a href="index.php?a=aboutUs" class="subnavbtn"><img src="assets/images/aboutIcon.png" alt="Über uns Bild">
+                <div class="topNavAbout">Über Uns</div>
+            </a>
         </li>
     </ul>
 </nav>
