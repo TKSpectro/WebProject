517a4f878d411e8b81e36e2ff40be50e1b69d792
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-12 10:42:46 +0100

    Update sideNav.php
    just formatting

--- views/shared/sideNav.php
+++ views/shared/sideNav.php
@@ -1,14 +1,14 @@
-<?php 
-namespace app\models; 
+<?php
+namespace app\models;
 ?>
 
 <div class="sidenav">
-<?php foreach(Cat::find() as $overCat):?> 
-<div class="overCat">
-<a href="index.php?a=products&type=Jungs-Toys&cat=<?=$overCat['catID'] ?>"><?=$overCat['descrip'];?></a>
-</div>   
-<?php foreach(ProdCat::find( 'catID = "'. $overCat['catID']. '"' ) as $rows):?>
-<a href="index.php?a=products&type=Jungs-Toys&prodCat=<?=$rows['prodCatID'] ?>"><?php echo $rows['descrip'];?></a>
-<?endforeach;?>
-<?endforeach;?>
+    <?php foreach (Cat::find() as $overCat): ?>
+        <div class="overCat">
+            <a href="index.php?a=products&type=Jungs-Toys&cat=<?= $overCat['catID'] ?>"><?= $overCat['descrip']; ?></a>
+        </div>
+        <?php foreach (ProdCat::find('catID = "' . $overCat['catID'] . '"') as $rows): ?>
+            <a href="index.php?a=products&type=Jungs-Toys&prodCat=<?= $rows['prodCatID'] ?>"><?php echo $rows['descrip']; ?></a>
+        <? endforeach; ?>
+    <? endforeach; ?>
 </div>
