ff5242ca5f3b1e68c56fc59e4e065daadb471d34
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-01-13 19:41:40 +0100

    Warenkorb-Changes

--- views/layout.php
+++ views/layout.php
@@ -6,10 +6,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/productList.css">
     <link rel="stylesheet" type="text/css" href="assets/css/sideNav.css">
     <link rel="stylesheet" type="text/css" href="assets/css/topNav.css">
-    <?php if(!empty($_GET))
-    {if($_GET['a']=='shoppingCart'):?>
     <link rel="stylesheet" type="text/css" href="assets/css/shoppingcart.css">
-    <?endif;}?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
