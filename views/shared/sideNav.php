a3fbe9be3fea070366fffe754635bf7ee24f6cce
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-14 12:20:22 +0100

    Update sideNav.php
    fixed SideNav

--- views/shared/sideNav.php
+++ views/shared/sideNav.php
@@ -5,10 +5,10 @@ namespace app\models;
 <div class="sidenav">
     <?php foreach (Cat::find() as $overCat): ?>
         <div class="overCat">
-            <a href="index.php?a=products&type=Jungs-Toys&cat=<?= $overCat['catID'] ?>"><?= $overCat['descrip']; ?></a>
+            <a href="index.php?a=products&type=<?=$_GET['type']?>&cat=<?= $overCat['catID'] ?>"><?= $overCat['descrip']; ?></a>
         </div>
         <?php foreach (ProdCat::find('catID = "' . $overCat['catID'] . '"') as $rows): ?>
-            <a href="index.php?a=products&type=Jungs-Toys&prodCat=<?= $rows['prodCatID'] ?>"><?php echo $rows['descrip']; ?></a>
+            <a href="index.php?a=products&type=<?=$_GET['type']?>&prodCat=<?= $rows['prodCatID'] ?>"><?php echo $rows['descrip']; ?></a>
         <? endforeach; ?>
     <? endforeach; ?>
 </div>
