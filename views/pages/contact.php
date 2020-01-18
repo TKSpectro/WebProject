1fed8bee7cf00064879ab0c681b59ee402c12f74
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-01-18 14:39:35 +0100

    Update contact.php
    changed contact formular to fit with others

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,1 +1,29 @@
-<h1>Kontakt Formular</h1>
+<div class="formular">
+    <form class="iamform" action="<?= $_SERVER['PHP_SELF'] . '?a=contact'; ?>" method="Post">
+
+        <figure>
+            <!--//TODO Change Picture-->
+            <img src="assets/images/Account_verwaltung2.png" alt="Account_verwaltung Bild">
+            <figcaption>Kontakt Formular</figcaption>
+        </figure>
+
+        <fieldset>
+            <legend>E-Mail *</legend>
+            <input type="email" name="eMail">
+        </fieldset>
+
+
+        <fieldset>
+            <legend>Betreff *</legend>
+            <input type="text" name="subject">
+        </fieldset>
+
+
+        <fieldset>
+            <legend>Nachricht *</legend>
+            <textarea id="message" name="message" placeholder="" style="height: 200px;"></textarea>
+        </fieldset>
+
+        <input type="submit" name="sendContact" value="Abschicken">
+    </form>
+</div>
