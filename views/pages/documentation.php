c657c4a9dcac1b483e2e4e49a0f3926b9e02c6bc
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-19 14:27:55 +0100

    Update documentation, add some pictures

--- views/pages/documentation.php
+++ views/pages/documentation.php
@@ -10,48 +10,57 @@
     <a href="#managment"><h2>8. Projektmanagment</h2></a>
     -------------------------------------------------------------------------------------------------------------------
     <a name="inhalt"><h2>1. Inhalt</h2></a>
-    <p>
-        Unser Kunde ist ToyPlanet, ein Online Spielzeugshop.<br>
-        Die Websitenutzer sind vorrangig Kinder und deren Eltern. Kinder können sich somit einfach auf der Website
-        zurecht finden<br>und die Produkte wählen die sie wollen.
-        Erwachsene sollen ebenfalls möglichst einfach Produkte finden und diese bestellen können.<br>
-    </p>
+    <ul>
+        <li>Unser Kunde ist ToyPlanet, ein Online Spielzeugshop.</li>
+        <li>Die Websitenutzer sind vorrangig Kinder und deren Eltern. Kinder können sich somit einfach auf der Website
+            zurecht finden<br>und die Produkte wählen die sie wollen.
+        </li>
+        <li>Erwachsene sollen ebenfalls möglichst einfach Produkte finden und diese bestellen können.</li>
+    </ul>
     <a name="recherche"><h2>2. Ergebnisse der Recherche</h2></a>
-    <p>
+    <ul>
         Als Referenz-Seiten haben wir uns an folgenden orientiert:
-        <a>1. </a>
-    </p>
+        <li>1.</li>
+    </ul>
     <a name="layout"><h2>3. Seitenlayout</h2></a>
-    <p>
-        WireFrames einfügen von lucidchart und vllt. AdobeXD Screenshots
-        SiteMap erstellen also navigationstruktur
-    </p>
+    <ul>
+        <li>WireFrames einfügen von lucidchart und vllt. AdobeXD Screenshots</li>
+        <img src="assets/images/documentation/WireFrame.png" style="height: 500px;">
+        <li>SiteMap:</li>
+        <img src="assets/images/documentation/SiteMap.png" style="height: 500px;">
+    </ul>
     <a name="design"><h2>4. Design</h2></a>
-    <p>
-        Layout:
-        Farben:
-        Schrift:
-        (Bilder?)
-    </p>
+    <ul>
+        <li>Layout:</li>
+        <li>Farben:</li>
+        <li>Schrift:</li>
+        <li>(Bilder?)</li>
+    </ul>
     <a name="function"><h2>5. Funktionalitäten</h2></a>
-    <p>
-        Screenshots für jede Seite mit Beschriftung
-    </p>
+    <ul>
+        <li>Screenshots für jede Seite mit Beschriftung</li>
+    </ul>
     <a name="ermodell"><h2>6. ER-Modell</h2></a>
-    <p>
-        ER-Modell Screenshot einfügen
-    </p>
+    <ul>
+        <li>ER-Modell Screenshot einfügen</li>
+    </ul>
     <a name="special"><h2>7. Besonderheiten</h2></a>
-    <p>
-        –Hinweise auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierungen, "versteckte" Zusatzfeatures...</p>
+    <ul>
+        <li>Hinweise auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierungen, "versteckte"
+            Zusatzfeatures...
+        </li>
+    </ul>
     <a name="managment"><h2>8. Projetkmanagment</h2></a>
-    <p>
-    <table style="width:100%">
-        <tr>
-            <th>Tätigkeit</th>
-            <th>Zuständiger</th>
-            <th>Aufwand</th>
-        </tr>
-    </table>
-    </p>
+    <ul>
+        <table style="width:100%">
+            <tr>
+                <th>Tätigkeit</th>
+                <th>Zuständiger</th>
+            </tr>
+            <tr>
+                <td>Dokumentation</td>
+                <td>Tom Käppler</td>
+            </tr>
+        </table>
+    </ul>
 </div>
