196b765d668a7cd7e38d2ee9e2d5e81e5b72f0a8
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-24 10:24:59 +0100

    Update aboutUs.php

--- views/pages/aboutUs.php
+++ views/pages/aboutUs.php
@@ -4,8 +4,8 @@ namespace app\models;
 <div class="aboutUsText">
     Wir sind Tom und Bilal, die Entwickler von TOYPLANET.<br>
     Weil Spielzeug vor allem Spaß machen soll, muss unsere Webseite diesen Spaß auch transportieren. <br>
-    Frohe Farben und im Gegensatz zu manchen Spielzimmern sehr aufgeräumt, sollen schon beim Einkaufen Freude
-    vermitteln.<br>
+    Frohe Farben und im Gegensatz zu manchen Spielzimmern sehr aufgeräumn. Dies soll schon beim Einkaufen Freude
+    bereiten.<br>
     Unsere Seite sieht simpel aus, hat aber die Funktionalität eines klassischen Online-Shops. <br>
     Sie finden hier Spielzeug für alle Altersgruppen und sogar Spiele für Erwachsene.<br>
     Hier im Universum des TOYPLANET wird ganz sicher auch jeder Erwachsene wieder zum Kind.
