a324643b49d2738eb904c281d5aea8ce1bf511d8
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-24 10:02:07 +0100

    fix css

--- views/pages/index.php
+++ views/pages/index.php
@@ -2,20 +2,20 @@
     <div class="homepage">
         <div class="startList">
             <a href="index.php?c=product&a=products&type=Jungs-Toys">
-                <img id="boy" src="assets/images/jung.png" alt="jung_image">
+                <img id="boyPicture" src="assets/images/jung.png" alt="jung_image">
                 <div class="overlay">Jungen</div>
                 
                 </a>
     </div>
         <div class="startList">
             <a href="index.php?c=product&a=products&type=Konsolespiele">
-            <img id="console" src="assets/images/vater.png" alt="vater_image">
+            <img id="consolePicture" src="assets/images/vater.png" alt="vater_image">
             <div class="overlay">Konsolen</div>
             </a>
         </div>
         <div class="startList">
             <a href="index.php?c=product&a=products&type=Mädchen-Toys">
-            <img id="girl" src="assets/images/girls.png" alt="girls_image">
+            <img id="girlPicture" src="assets/images/girls.png" alt="girls_image">
             <div class="overlay">Mädchen</div>
             </a>
         </div>
