07504adb7a36fb14935fddc6e2f5f91fe87fdea3
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-24 10:12:05 +0100

    fixed core withespacing and codestyle

--- core/controller.php
+++ core/controller.php
@@ -6,50 +6,44 @@ namespace app\core;
 
 class Controller
 {
-	private $_actionName = null;
-	private $_controllerName = null;
+    private $_actionName = null;
+    private $_controllerName = null;
 
-	protected $_params = [];
+    protected $_params = [];
 
-	public function __construct($actionName = null, $controllerName = null)
-	{
-		$this->_actionName = $actionName;
-		$this->_controllerName = $controllerName;
-	}
+    public function __construct($actionName = null, $controllerName = null)
+    {
+        $this->_actionName = $actionName;
+        $this->_controllerName = $controllerName;
+    }
 
-	public function renderHTML()
-	{
-		$viewPath = $this->viewPath($this->_actionName, $this->_controllerName);
-		
+    public function renderHTML()
+    {
+        $viewPath = $this->viewPath($this->_actionName, $this->_controllerName);
 
-		if(file_exists($viewPath))
-		{
-			extract($this->_params);
-			
-			$body = '';
 
-			ob_start();
-			{
-				
-				include $viewPath;
-				
+        if (file_exists($viewPath))
+        {
+            extract($this->_params);
 
-			}
-			$body = ob_get_clean();
-		
-				include __DIR__.'/../views/layout.php';
-			
-		}
-		else
-		{
-			header('Location: index.php?c=pages&a=error404');
-		}
-	}
+            $body = '';
 
-	protected function viewPath($actionName = null, $controllerName = null)
-	{
-		
-		return __DIR__.'/../views/pages/'.$actionName.'.php';
-		
-	}
+            ob_start();
+            {
+                include $viewPath;
+            }
+            $body = ob_get_clean();
+
+            include __DIR__ . '/../views/layout.php';
+        }
+        else
+        {
+            header('Location: index.php?c=pages&a=error404');
+        }
+    }
+
+    protected function viewPath($actionName = null, $controllerName = null)
+    {
+        return __DIR__ . '/../views/pages/' . $actionName . '.php';
+    }
 }
--- core/function.php
+++ core/function.php
@@ -1,44 +1,37 @@
 <?
-
-
 function checkIfRemembered()
 {
-    
-      $email   = $_COOKIE['email'];
-      $password=$_COOKIE['password'];
-      $where= \app\models\Account::find('email = "'.$email. '"');
-      	
-      if(!empty($email) && !empty($password))
-      {
-        if(!empty($where))
-         {
-            if(password_verify($password, $where['0']['passwordHash']))
+    $email = $_COOKIE['email'];
+    $password = $_COOKIE['password'];
+    $where = \app\models\Account::find('email = "' . $email . '"');
+
+    if (!empty($email) && !empty($password))
+    {
+        if (!empty($where))
+        {
+            if (password_verify($password, $where['0']['passwordHash']))
             {
-              $_SESSION['loggedIn'] = true; 
-              
-              include __DIR__ . '/../controller/shared/shoppingcartHelper.php';
-            }      
-         }
-      }
-      elseif(isset($_SESSION['accountID']))
-      {
-            $_SESSION['loggedIn'] = true; 
-              
-            include __DIR__ . '/../controller/shared/shoppingcartHelper.php';
-      }
+                $_SESSION['loggedIn'] = true;
+                include __DIR__ . '/../controller/shared/shoppingcartHelper.php';
+            }
+        }
+    }
+    elseif (isset($_SESSION['accountID']))
+    {
+        $_SESSION['loggedIn'] = true;
+        include __DIR__ . '/../controller/shared/shoppingcartHelper.php';
+    }
 }
 
 function rememberMe($email, $password)
 {
-      $duration = time() + 3600 * 24 * 30;
-      setcookie('email' , $email, $duration,'/');
-      setcookie('password' , $password, $duration,'/');
-    
+    $duration = time() + 3600 * 24 * 30;
+    setcookie('email', $email, $duration, '/');
+    setcookie('password', $password, $duration, '/');
 }
 
 function findMyAdress()
 {
-      app\models\Address::find('land = "'.$land. '" and city = "'.$city. '"
-             and street = "'.$street. '" and houseNumber = "'.$houseNumber. '" and zip = "'.$zip. '"');
-           
+    app\models\Address::find('land = "' . $land . '" and city = "' . $city . '"
+             and street = "' . $street . '" and houseNumber = "' . $houseNumber . '" and zip = "' . $zip . '"');
 }
