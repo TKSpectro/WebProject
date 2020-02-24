39965338a9883b954dd74a33c6fc42a2622b5234
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-02-24 11:05:09 +0100

    some documentation stuff

--- views/pages/documentation.php
+++ views/pages/documentation.php
@@ -11,10 +11,9 @@
     -------------------------------------------------------------------------------------------------------------------
     <a name="inhalt"><h2>1. Inhalt</h2></a>
     <ul>
-        <li>Unser Kunde ist ToyPlanet, ein Online Spielzeugshop.</li>
-        <li>Die Websitenutzer sind vorrangig Kinder und deren Eltern. Kinder können sich somit einfach auf der Website
-            zurecht finden<br>und die Produkte wählen die sie wollen.
-        </li>
+        <li>Wir sind ToyPlanet, ein Online Spielzeugshop.</li>
+        <li>Die Websitenutzer sind vorrangig Kinder und deren Eltern.</li>
+        <li>Kinder können sich somit einfach auf der Website zurecht finden und die Produkte wählen die sie wollen.</li>
         <li>Erwachsene sollen ebenfalls möglichst einfach Produkte finden und diese bestellen können.</li>
     </ul>
     <a name="recherche"><h2>2. Ergebnisse der Recherche</h2></a>
@@ -24,7 +23,7 @@
     </ul>
     <a name="layout"><h2>3. Seitenlayout</h2></a>
     <ul>
-        <li>WireFrames einfügen von lucidchart und vllt. AdobeXD Screenshots</li>
+        <li>WireFrames:</li>
         <img src="assets/images/documentation/WireFrame.png" style="height: 500px;">
         <li>SiteMap:</li>
         <img src="assets/images/documentation/SiteMap.png" style="height: 500px;">
@@ -32,8 +31,13 @@
     <a name="design"><h2>4. Design</h2></a>
     <ul>
         <li>Layout: Möglichst einfach gehalten, aber trotzdem leicht verspielt.</li>
-        <li>Farben: Cremefarbe für den Hintergrund und für die jeweiligen Kategorien jeweils eine zugewiesene Farbe für eine schnelle Unterscheidung</li>
-        <li>Schrift: Unsere Standard Schriftart ist "" . Für Login, Registrierung oder Lange Fließtexte wird "" verwendet um ein einfachereres lesen zu ermöglichen</li>
+        <li>Farben: Cremefarbe für den Hintergrund und für die jeweiligen Kategorien jeweils eine zugewiesene Farbe für
+            eine schnelle Unterscheidung
+        </li>
+        <li>Schrift: Unsere Standard Schriftart ist "KGILikeToMoveIt" . Für Login, Registrierung oder Lange Fließtexte
+            wird "Roboto-Regular"
+            verwendet um ein einfachereres lesen zu ermöglichen
+        </li>
         <li>Bilder: Möglichst farbenfroh und passend zum Thema</li>
     </ul>
     <a name="function"><h2>5. Funktionalitäten</h2></a>
@@ -42,27 +46,59 @@
         <ul>
             <li>Login, Registrierung, Accountdaten ändern</li>
             <li>Produktseite, Einzelproduktansicht, Produktfilterung, Produktsuche</li>
-            <li>Warenkorb, Angedeutete Bezahlung</li>
+            <li>Warenkorb, Bestellvorgang, Angedeutete Bezahlung</li>
             <li>Kontakt-Formular</li>
         </ul>
-        <li>Screenshots für jede Seite mit Beschriftung</li>
+
+        <li>Flowchart für den Bestell-/Login-/Registierungsvorgang:</li>
+        <img src="assets/images/documentation/orderFlowChart.png" style="height: 800px; width: auto;">
+        <br><br><br><br>
+
+        <li>Die Seiten für Login, Registrierung und Accountdaten sind selbsterklärend</li>
+        <img src="assets/images/documentation/webSitePictures/login.png"
+             style="height:800px; width: auto; border: 1px solid black;">
+        <br><br><br><br>
+
+        <li>Produktseite</li>
+        <img src="assets/images/documentation/webSitePictures/products.png"
+             style="height:800px; width: auto; border: 1px solid black;">
+        <br><br><br><br>
+
+        <li>Warenkorb</li>
+        <img src="assets/images/documentation/webSitePictures/shoppingCart.png"
+             style="height:800px; width: auto; border: 1px solid black;">
+        <br><br><br><br>
+        <li>Bestellen falls man nicht eingeloggt ist</li>
+        <img src="assets/images/documentation/webSitePictures/checkOutNotLoggedIn.png"
+             style="height:800px; width: auto; border: 1px solid black;">
+        <br><br><br><br>
     </ul>
     <a name="ermodell"><h2>6. ER-Modell</h2></a>
     <ul>
-        <li>ER-Modell Screenshot einfügen</li>
+        <img src="assets/images/documentation/er.png">
     </ul>
     <a name="special"><h2>7. Besonderheiten</h2></a>
     <ul>
-        <li>Hinweise auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierungen, "versteckte"
-            Zusatzfeatures...
-        </li>
+        <li></li>
     </ul>
     <a name="managment"><h2>8. Projetkmanagment</h2></a>
     <ul>
-        <table style="width:100%">
+        <table style="width:500px">
             <tr>
                 <th>Tätigkeit</th>
                 <th>Zuständiger</th>
+            </tr>
+            <tr>
+                <td>Datenstruktur</td>
+                <td>Tom Käppler</td>
+            </tr>
+            <tr>
+                <td>Aussehen/CSS</td>
+                <td>Bilal Alnaani</td>
+            </tr>
+            <tr>
+                <td>Funktionalität</td>
+                <td>Beide</td>
             </tr>
             <tr>
                 <td>Dokumentation</td>
