ced97777b9332b00b18c92b9534563928ddd1489
bilal.alnaani@fh-erfurt.de <bilal.alnaani@fh-erfurt.de>
2020-10-08 16:12:07 +0200

    Heroku DB-Einstellung

--- init/10_database.php
+++ init/10_database.php
@@ -1,9 +1,10 @@
 <?php
 
 
-$dns = 'mysql:host=localhost;dbname=toyplanet';
-$dbuser = 'root';
-$dbpassword = '';
+$dns = 'mysql:host=lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com
+;dbname=e74qmxfxv3q3smz7';
+$dbuser = 'vqcf16ic8fegyfvr';
+$dbpassword = 'q7d29ciomxcvp8yo';
 $options    = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
